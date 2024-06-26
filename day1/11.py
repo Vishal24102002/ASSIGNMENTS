72c4603c424954ed981f1de779e16a9baa1a109c
Vishal sharma <103731436+Vishal24102002@users.noreply.github.com>
2024-06-26 17:07:58 +0530

    Create 11.py

---
+++ day1/11.py
@@ -0,0 +1,9 @@
+#python
+
+#funtion creation
+def sum(a,b):
+  #return the string containing theb sum of the number 
+  return(f"Sum of the number is {a+b}")
+
+#funtion calling in the print statement itself
+print(sum(1,8))
