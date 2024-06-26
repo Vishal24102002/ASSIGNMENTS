54732a061fd703f91398c9619fe7d664d75af305
Vishal sharma <103731436+Vishal24102002@users.noreply.github.com>
2024-06-26 16:36:49 +0530

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,8 @@
+#python
+
+n=int(input("enter the number of natural numbers to be printed"))
+print("natural numbers are")
+#for loop for printing the natural numbers
+for i in range(0,n):
+  #print i+1 as natural numbers starts from 1 but i is starting from 0 
+  print(i+1,"\n")
