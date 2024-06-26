850756629831d08f840c61165b8d675f22711dad
Vishal sharma <103731436+Vishal24102002@users.noreply.github.com>
2024-06-26 16:28:32 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,1 +1,14 @@
+#PYTHON 
 
+#user input
+a=int(input("enter the number"))
+
+#condition for checking value of a is zero or not
+if a==0:
+  print("a is zero")
+#if a is less than 0 then it is negative
+elif a<0:
+  print("negative")
+#else it is positive. if the above conditions are not satisfied then only 
+else:
+  print("positive")
