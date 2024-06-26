04089905d2a949183078ac712fd32f9ae115fae8
Vishal sharma <103731436+Vishal24102002@users.noreply.github.com>
2024-06-26 16:46:46 +0530

    Update 7.py

--- day1/7.py
+++ day1/7.py
@@ -1,7 +1,11 @@
 #python
 
+#taking user input
 table=int(input("enter the number for table"))
+#initializing the value of i to 1
 i=1
 while i<11:
+  #printing the table format 
   print(f"{table} X {i} = {i*table}")
+  #incrementing the value of i to i+1
   i=i+1
